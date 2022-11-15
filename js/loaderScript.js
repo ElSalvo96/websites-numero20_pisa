c863230cc93ddaddfdab1a7d030649783a886fcf
ElSalvo96 <74384157+ElSalvo96@users.noreply.github.com>
2022-11-15 01:32:20 +0100

    add loader effects + reduce image size

--- js/loaderScript.js
+++ js/loaderScript.js
@@ -2,15 +2,29 @@ const startTime = new Date().getTime();
 const minLoaderTimer = 3000;
 window.addEventListener('load', (event) => {
 	const doAction = () => {
-		document.getElementById('content').classList.remove('invisible');
-		document.getElementById('loader').remove();
-		document.getElementById('loaderStylesheet').remove();
-		document.getElementById('loaderScript').remove();
+		const loader = document.getElementById('loader');
+
+		const elements = $('.invisible');
+		elements.removeClass('invisible');
+		document.body.classList.add('active-effects');
+
+		loader.classList.remove('rotate-center');
+		loader.classList.add('slide-out-fwd-center');
+		$('.carousel').carousel({
+			ride: true,
+		});
+
+		setTimeout(() => {
+			loader.remove();
+			document.getElementById('loaderStylesheet').remove();
+			document.getElementById('loaderScript').remove();
+			const elements = $('.animate');
+			elements.removeClass('animate');
+		}, 3000);
 	};
 
 	const endTime = new Date().getTime();
 	const gap = endTime - startTime;
-	console.log(gap, minLoaderTimer, gap < minLoaderTimer, minLoaderTimer - gap);
 	if (gap < minLoaderTimer) setTimeout(doAction, minLoaderTimer - gap);
 	else doAction();
 });
