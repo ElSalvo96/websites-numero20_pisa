2d999bccf1ac1972dfbcf784ca9750b1caea6245
FSA <fabio.salvini@rifranet.it>
2022-11-25 16:27:55 +0100

    fix text + optimize html build

--- build.bat
+++ build.bat
@@ -7,13 +7,16 @@ rmdir /s /q %destinationDir%
 mkdir %destinationDir%\%copyDirCss% 
 mkdir %destinationDir%\%copyDirJs% 
 @REM robocopy %copyDirCss% %destinationDir%/%copyDirCss% /E /is /it
-@REM robocopy %copyDirJs% %destinationDir%/%copyDirJs% /E /is /it
+robocopy %copyDirJs% %copyDirJs%/temp /E /is /it
+del %copyDirJs%\temp\menu.ts
+
 robocopy %copyDirResources% %destinationDir%/%copyDirResources% /E /is /it
 
 for %%f in (%copyDirCss%/*) do call uglifycss %copyDirCss%/%%f > %destinationDir%/%copyDirCss%/%%f
-for %%f in (%copyDirJs%/*) do call uglifyjs %copyDirJs%/%%f > %destinationDir%/%copyDirJs%/%%f
+for %%f in (%copyDirJs%/temp/*) do call uglifyjs %copyDirJs%/temp/%%f > %destinationDir%/%copyDirJs%/%%f
 
-robocopy ./ %destinationDir% index.html /is /it
 
-del %destinationDir%\%copyDirJs%\menu.ts
+html-minifier index.html --remove-comments --collapse-whitespace --minify-js --minify-css > %destinationDir%\index.html
+
 rmdir /s /q %destinationDir%\%copyDirResources%\original
+rmdir /s /q %copyDirJs%\temp
